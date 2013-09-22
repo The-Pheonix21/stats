4c856dd46f1ba5c6264bdfd3d77074158672d9c8
Chris <c5keith90@gmail.com>
2013-09-22 13:09:21 -0600

    yeper

--- stats.java
+++ stats.java
@@ -4,14 +4,16 @@ public static void main(String[] args) {
 		int[] b = {5,4,63,45,6};
 		int[] artyom = {1,2,3,4,5,6};
 		int[] a = {1,2,3,4,5,6,7,8,9,10};
-		double[] ar = {1.1,1.3,1.6,2.2,5.2,6.1,7.5};
-		System.out.println(max(artyom));
-		System.out.println(min(b));
-		System.out.println(mean(ar));
-		System.out.println(median(ar));
-		System.out.println(mode(a));
-		System.out.println(standardDev(ar));
-		System.out.println(Quartile(ar,3));
+		double[] ar = {1.1,1.3,1.6,2.2,5.2,6.1,7.5,8.0};
+		System.out.println("max " +max(artyom));
+		System.out.println("min: " +min(b));
+		System.out.println("mean is " +mean(ar));
+		System.out.println("Median equals "+median(ar));
+		System.out.println("Mode =" +mode(a));
+		System.out.println("Standard Dev "+standardDev(ar));
+		System.out.println("Quartile 1: "+Quartile(ar,1));
+		System.out.println("Quartile 2: "+Quartile(ar,2));
+		System.out.println("Quartile 3: "+Quartile(ar,3));
 	}	
 	public static int max(int[] artyom) {
 		int max = artyom[0];
@@ -48,14 +50,31 @@ public static void main(String[] args) {
     	}
 	}
 	public static double Quartile(double[] ar, int qt){
-				int quartval = qt*(ar.length/4);
-				if (ar.length%4 == 0){
-					System.out.println("quartval = "+ quartval);
-					return (ar[(quartval-1)]*.25)+(ar[((quartval))]*.75);
-				}else{
-					return (ar[quartval]+ar[quartval+1])/2.0;
-					}	 
+		int q1 = (ar.length+1)/4;
+		int q3 = ((ar.length+1)/4)*3;
+		if (qt==2) {
+		return median(ar);
+		}
+		if (qt==1) {
+			if ((ar.length+1)%4 == 1) {
+				//odd
+				return (ar[q1-1]+ar[q1])/2.0;
+			}else {
+				//even
+				return ar[q1-1];
 			}
+		}if (qt==3) {
+			if ((ar.length+1)%4 == 1) {
+				//odd
+				return (ar[q3-1]+ar[q3])/2.0;
+			}else {
+				//even
+				return ar[q3-1];
+			}
+		}
+		return 0.0;
+	}
+
 		public static int mode(int[] a) {
     	int maxValue=0;
     	int maxCount=0;
@@ -82,3 +101,39 @@ public static void main(String[] args) {
 		return Math.sqrt(temp);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
